6f6b8a298cffae1fd23c3cff3a70d01dda74d0db
grohovski_igor <i.grokhovskij@dmaby.com>
2023-04-15 18:13:41 +0300

    редактирование страницы

---
+++ src/sections/applicants/component/action.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {TitleComponent} from "../../../components/elements/title/title.component";
+import ButtonComponent from "../../../components/elements/button/button.component";
+import "./action.style.css";
+
+const Action = () => {
+    return (
+        <div id="content">
+            <TitleComponent text="Применить действие"/>
+
+            <div className="buttonBlock">
+                <ButtonComponent text="Удалить"/>
+                <ButtonComponent text="Рассылка"/>
+            </div>
+        </div>
+    );
+};
+
+export default Action;
--- src/sections/applicants/component/headerApplicants.tsx
+++ src/sections/applicants/component/headerApplicants.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TitleComponent} from "../../../components/elements/title/title.component";
 import ButtonComponent from "../../../components/elements/button/button.component";
 import "./headerApplicants.style.css";
+import ModalWrapperComponent from "../../../components/modalWrapper/modalWrapper.component";
+import Action from "./action";
 
 const HeaderApplicants = () => {
 
-    const visibility = () => {
-        console.log(1);
+    const [modal, setModal] = useState<JSX.Element | null>(null);
+    const handleClick = () => {
+        const inputList: NodeListOf<HTMLInputElement> = document.querySelectorAll(".checkBox");
+
+        inputList.forEach((item: HTMLInputElement): void => {
+            if (item.checked) {
+                setModal(<ModalWrapperComponent component={<Action/>} setState={setModal}/>)
+            }
+        })
     }
 
 
@@ -14,9 +23,12 @@ const HeaderApplicants = () => {
         <>
             <div className="header-applicants">
                 <TitleComponent text="Соискатели"/>
-                <div className="filter">Фильтер</div>
-                <ButtonComponent text="Массовое действие" onClick={visibility}/>
+                <div className="filter">Фильтр</div>
+
+                <ButtonComponent text="Массовое действие" onClick={handleClick}/>
             </div>
+
+            {modal}
         </>
     );
 };
